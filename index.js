9a84b090a05f97b8d2c945d6b6c7ccab041b0131
AlexIrungu <aleaxmuiruri@gmail.com>
2022-12-14 14:25:51 +0300

    Completed code challenge wk-3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:3000/films'
 const listHolder = document.getElementById('films')
 document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementsByClassName('film item')[0].remove()
+    document.getElementsByClassName('film-item')[0].remove()
     fetchMovies(url)
 })
 //Create fetch function to get the data from the db.json
@@ -27,7 +27,7 @@ function addClickEvent(){
     let children=listHolder.children
     for(let i=0; i<children.length; i++){
         let child=children[i]
-        // console.log(child) <= to check if have the right child
+        // console.log(child) => to check if have the right child
         child.addEventListener('click',() => {
             fetch(`${url}/${i+1}`)
             .then(res => res.json())
@@ -43,23 +43,27 @@ function addClickEvent(){
 function setUpMovieDetails(funMovie){
     const preview = document.getElementById('movie-poster')
     preview.src = funMovie.poster;
-//title
+    //title
     const movieTitle = document.querySelector('#title');
     movieTitle.textContent = funMovie.title;
+
     //runtime
     const movieTime = document.querySelector('#runtime');
     movieTime.textContent = `${funMovie.runtime} minutes`;
+
     //description
     const movieDescription = document.querySelector('#film-info');
     movieDescription.textContent = funMovie.description;
-    //Showtime
+
+    //showtime
     const showTime = document.querySelector('#showtime')
     showTime.textContent = funMovie.showtime;
-    // available tickets =capacity - tickets sold
+
+    //available tickets =capacity - tickets sold
     const tickets  = document.querySelector('#ticket-number')
     tickets.textContent = funMovie.capacity -funMovie.tickets_sold;
 }
-// //Sold out
+ //Sold out
 const btn = document.getElementById('buy-ticket')
         btn.addEventListener('click', function(event){
             let remainingTickets = document.querySelector('#ticket-number').textContent
@@ -71,31 +75,4 @@ const btn = document.getElementById('buy-ticket')
                 btn.textContent = 'Sold Out'
             }
     })
- //
-// function displayFirstMovie(loadFirstMovie){
-//     const preview = document.getElementById('movie-poster')
-//     preview.src = loadFirstMovie.poster;
-// //title
-//     const movieTitle = document.querySelector('#title');
-//     movieTitle.textContent = loadFirstMovie.title;
-//     //runtime
-//     const movieTime = document.querySelector('#runtime');
-//     movieTime.textContent = `${loadFirstMovie.runtime} minutes`;
-//     //description
-//     const movieDescription = document.querySelector('#film-info');
-//     movieDescription.textContent = loadFirstMovie.description;
-//     //Showtime
-//     const showTime = document.querySelector('#showtime')
-//     showTime.textContent = loadFirstMovie.showtime;
-//     // available tickets =capacity - tickets sold
-//     const tickets  = document.querySelector('#ticket-number')
-//     tickets.textContent = loadFirstMovie.capacity -funMovie.tickets_sold;
-// }
-
-// function loadFirstMovie(nameMovie){
-//     fetch(url)
-//         .then(res=>res.json())
-//         .then((data)=>{
-            
-//         })
-// }
+ 
